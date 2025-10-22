c1913c6e07c9287b851e77c0bdc94222f5b20358
Twisha Bansal <twishabansal07@gmail.com>
2025-10-22 22:15:29 +0530

    fix

--- packages/toolbox-core/src/toolbox_core/utils.ts
+++ packages/toolbox-core/src/toolbox_core/utils.ts
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-type BoundValue = unknown | (() => unknown) | (() => Promise<unknown>);
+export type BoundValue = unknown | (() => unknown) | (() => Promise<unknown>);
 
-type BoundParams = Record<string, BoundValue>;
+export type BoundParams = Record<string, BoundValue>;
 type RequiredAuthnParams = Record<string, string[]>;
 
 /**

092a098856d532f6e047beb6c6f0e297c9262ee8
Twisha Bansal <twishabansal07@gmail.com>
2026-03-03 14:36:13 +0530

    accomodate major update

--- packages/toolbox-adk/.prettierrc.cjs
+++ packages/toolbox-adk/.prettierrc.cjs
@@ -16,7 +16,6 @@
 // ** https://github.com/googleapis/gapic-generator-typescript **
 // ** All changes to this file may be overwritten. **
 
-
 module.exports = {
-    ...require('gts/.prettierrc.json')
-}
+  ...require('gts/.prettierrc.json'),
+};
